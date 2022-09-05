dbc914dcc8ddf522c97c32a9235751aba392ddda
Song Zhaohui <zhaohui.song@studio.unibo.it>
2022-09-05 21:47:51 +0200

    add tests for simple account

--- ZhaohuiSong/Budmate/Budmate/SimpleAccount.cs
+++ ZhaohuiSong/Budmate/Budmate/SimpleAccount.cs
@@ -1,12 +1,14 @@
 using System;
+using System.Collections.Generic;
 
 namespace Budmate
 {
     public class SimpleAccount : BaseAccount
     {
+
         private readonly string _id;
         
-        public SimpleAccount(string id, double amount)
+        public SimpleAccount(string id, double amount = 0)
         {
             _id = id;
            Deposit(amount);
@@ -20,5 +22,33 @@ namespace Budmate
 
         public override string GetId() => _id;
 
+        /// <summary>
+        /// Overloading operator to compare balances
+        /// </summary>
+        /// <param name="a">first account</param>
+        /// <param name="b">second account</param>
+        /// <returns>if they have equal balance</returns>
+        public static bool operator ==(SimpleAccount a,SimpleAccount b) =>
+             Math.Abs(a.GetBalance() - b.GetBalance()) < 0.01;
+        
+
+        public static bool operator !=(SimpleAccount a, SimpleAccount b) =>
+             Math.Abs(a.GetBalance() - b.GetBalance()) > 0.01;
+        
+        
+        private bool Equals(SimpleAccount other) => _id == other._id;
+        
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SimpleAccount) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (_id != null ? _id.GetHashCode() : 0);
+        }
     }
 }
--- ZhaohuiSong/Budmate/Budmate/Tests.cs
+++ ZhaohuiSong/Budmate/Budmate/Tests.cs
@@ -1,14 +1,33 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
 
 namespace Budmate
 {
     [TestFixture]
     public class Tests
     {
+        
         [Test]
-        public void Test1()
+        public void TestEquality()
         {
-            Assert.True(true);
+            IAccount acc1 = new SimpleAccount("uni credit");
+            IAccount acc2 = new SimpleAccount("san polo", 0);
+            IAccount acc3 = new SimpleAccount("san polo", 200);
+            Assert.IsFalse(acc1.Equals(acc2));
+            Assert.IsTrue(acc2.Equals(acc3));
+            Assert.IsFalse(acc1.Equals(acc3));
+        }
+        
+        [Test]
+        public void TestEqualBalance()
+        {
+            var acc1 = new SimpleAccount("uni credit");
+            var acc2 = new SimpleAccount("san polo", 24.2);
+            acc1.Deposit(24.2);
+            Assert.IsTrue(acc1 == acc2);
+            acc1.Withdraw(24);
+            Assert.IsTrue(acc1 != acc2);
         }
     }
 }
